fb2b38a3aafde6287763c72e81ed667565114024
3000ye <hcg2924239214@163.com>
2024-04-27 19:10:24 +0800

    Initial Commit

---
+++ FirstSwiftApp/FirstSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstSwiftAppApp.swift
+//  FirstSwiftApp
+//
+//  Created by Castor on 2024/4/27.
+//
+
+import SwiftUI
+
+@main
+struct FirstSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
